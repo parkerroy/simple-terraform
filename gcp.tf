3cf105f5a01012ca6f6a6db1b3ece30905193b78
parkerroy <69988242+parkerroy@users.noreply.github.com>
2021-03-02 19:38:29 -0600

    Create gcp.tf

---
+++ gcp.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("<SERVICEACCOUNTKEY>.json")
+
+  project = "PROJECTID"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "appserver" {
+  name = "billy-application-server"
+  machine_type = "f1-micro"
+
+  boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+}
+ network_interface {
+   network = "default"
+}
+}
